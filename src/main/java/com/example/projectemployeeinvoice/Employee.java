2b0a6dfafa8fcc4c46fab9d5881ecfaee0c75dde
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2024-01-30 13:21:39 +0100

    Update Employee.java

--- src/main/java/com/example/projectemployeeinvoice/Employee.java
+++ src/main/java/com/example/projectemployeeinvoice/Employee.java
@@ -5,19 +5,6 @@ public class Employee {
     private String username;
     private String password;
 
-    // Constructor
-    public Employee(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-
-    // En ny konstruktor som bara tar emot username och password
-    public Employee(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 
 
     // Getters and Setters
