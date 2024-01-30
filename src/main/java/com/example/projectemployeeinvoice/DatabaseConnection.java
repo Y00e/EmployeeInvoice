5eeb7a0e183a0e78319bf753ab95b9b8ca66a674
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2024-01-30 13:52:55 +0100

    Update DatabaseConnection.java

--- src/main/java/com/example/projectemployeeinvoice/DatabaseConnection.java
+++ src/main/java/com/example/projectemployeeinvoice/DatabaseConnection.java
@@ -3,25 +3,17 @@ package com.example.projectemployeeinvoice;
 import java.sql.*;
 
 public class DatabaseConnection {
-    private String url = "jdbc:mysql://localhost:3306/EmployeeInvoice";
-    private String user = "root";
-    private String password = "";
 
-    public Connection connect() {
-        Connection conn = null;
+    
+    private Connection conn;
+
+    public DatabaseConnection() {
         try {
-            // Lägg till denna rad för att ladda drivrutinen
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the database!");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Oops! Couldn't find the JDBC driver." + e.getMessage());
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.out.println("Oops! Something went wrong." + e.getMessage());
-            e.printStackTrace();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/EmployeeInvoice", "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
         }
-        return conn;
     }
 
 
