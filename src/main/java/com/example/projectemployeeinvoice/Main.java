6e649ba82b4ae9fb73541d3e08805618e4d0bf56
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2024-02-12 09:12:01 +0100

    Delete src/main/java/com/example/projectemployeeinvoice/Main.java

--- src/main/java/com/example/projectemployeeinvoice/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.projectemployeeinvoice;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseConnection dbConnection = new DatabaseConnection();
-        dbConnection.connect();
-    }
-}
